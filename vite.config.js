de705fdea66f3049f732c93155c26eed0cfad746
SoumyaShri1209 <soumya12shri@gmail.com>
2025-08-20 22:21:42 +0530

    Update base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+   base: '/agency-ai-frontend/',
   plugins: [
      react() ,
      tailwindcss()
